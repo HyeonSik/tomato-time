c616a330b1e6991a768d3a57d4b03eee375f2d38
hyunsik <shs9589@gmail.com>
2018-03-26 02:56:14 +0900

    Finish

--- component/Timer/presenter.js
+++ component/Timer/presenter.js
@@ -2,20 +2,47 @@ import React, { Component } from "react";
 import { View, Text, StyleSheet, StatusBar } from "react-native";
 import Button from "../Button";
 
+function formatTime(time) {
+  let minutes = Math.floor(time / 60);
+  time -= minutes * 60;
+  let seconds = parseInt(time % 60, 10);
+  return ` ${minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
+}
+
 class Timer extends Component {
+  componentWillReceiveProps(nextProps) {
+    const currentProps = this.props;
+    if (!currentProps.isPlaying && nextProps.isPlaying) {
+      const timeInterval = setInterval(() => {
+        currentProps.addSecond();
+      }, 1000);
+      this.setState({
+        timeInterval
+      });
+    } else if (currentProps.isPlaying && !nextProps.isPlaying) {
+      clearInterval(this.state.timeInterval);
+    }
+  }
+
   render() {
+    console.log(this.props);
     const {
       isPlaying,
       elapsedTime,
       timeDuration,
       startTimer,
-      restartTimer
+      restartTimer,
+      addSecond
     } = this.props;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <View style={styles.upper}>
-          <Text style={styles.text}>25:00</Text>
+          <Text style={styles.text}>
+            {formatTime(timeDuration - elapsedTime)}
+          </Text>
         </View>
         <View style={styles.lower}>
           {!isPlaying && <Button iconName="play-circle" onPress={startTimer} />}
